8da00b975ec62c709ef208a759f375e7a43a245c
Srivardhan Karnati <srivar.karnati@gmail.com>
2023-09-25 12:03:52 -0500

    added MVP

--- Final_project_484/src/ExcalidrawPNG.jsx
+++ Final_project_484/src/ExcalidrawPNG.jsx
@@ -13,7 +13,9 @@ function LocalImage() {
         width={600}
         height={300}
       />
-      <b> @#&$ </b>
+      <div>
+        <p> Our MVP is going to be a mental journaling app geared towards CS students which contains components from the Chat GPT to provide AI-generated conversation. Users will be able to document their emotions, experience, and feelings, in which that will help the AI to provide feedback towards that user. </p>
+      </div>
     </div>
   );
 }
