f8fb6330cc12aacec5543a571e5a2136d6b7bedd
Anthony Sorto <asorto3@uic.edu>
2023-09-24 21:26:33 -0500

    next

--- Final_project_484/src/main.jsx
+++ Final_project_484/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ExcalidrawEmbed from './ExcalidrawEmbedded.jsx'
+import LocalSVG from './ExcalidrawSVG.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ExcalidrawEmbed />
+    <LocalSVG />
   </React.StrictMode>,
 )
